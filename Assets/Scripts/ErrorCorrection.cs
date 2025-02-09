607a54a710af32d2a34e1f0921528c4c69354be6
Hakan Sarıoğlu <61359973+hakansa00219@users.noreply.github.com>
2025-02-09 23:40:42 +0300

    still giving wrong data for ec. working on it

--- Assets/Scripts/ErrorCorrection.cs
+++ Assets/Scripts/ErrorCorrection.cs
@@ -29,19 +29,29 @@ namespace QR
         {
             // Ver1 Byte High EC 17 bytes
             ReedSolomonGenerator generator = new ReedSolomonGenerator();
-            var poly = generator.Generate(_ecDataSize);
-
-            int[] data = _data.ToCharArray().Select(c => (int)c).ToArray();
-
-            int[] errorCorrectionData = generator.ComputeErrorCorrectionCodewords(data, poly, _ecDataSize);
             
-            for (int i = 0; i < _ecDataSize; i++) //17 characters
+            // byte[] dataArray = _data.ToCharArray().Select(x => (byte)x).ToArray();
+            byte[] dataArray =
+            {
+                0b01000000,
+                0b01000101,
+                0b01100110,
+                0b01010111,
+                0b00100011,
+                0b00010000,
+                0b11101100,
+                0b00010001,
+                0b11101100
+            };
+            
+            var ecblocks = generator.GenerateECBlocks(dataArray, _ecDataSize);
+            
+            for (int i = 0; i < _ecDataSize; i++) //17 characters 
             for (int j = ERROR_CORRECTION_LENGTH - 1; j >= 0; j--) //8 bits for each character
             {   
                 var bitNode = _analyzer.BitQueue.Dequeue();
-                _texture.SetPixel2D(bitNode.X, bitNode.Y, ((errorCorrectionData[i] >> j) & 1) == 1 ? Color.black : Color.white);
-                if(i == 16 && j == 0)
-                    Debug.Log($"({bitNode.X}, {bitNode.Y})");
+                _texture.SetPixel2D(bitNode.X, bitNode.Y, ((ecblocks[i] >> j) & 1) == 1 ? Color.black : Color.white);
+                Debug.Log(((ecblocks[i] >> j) & 1) == 1 ? "1" : "0");
             }
         }
         
--- Assets/Scripts/ReedSolomonGenerator.cs
+++ Assets/Scripts/ReedSolomonGenerator.cs
@@ -6,92 +6,106 @@ namespace QR
     public class ReedSolomonGenerator
     {
         private readonly int PRIMITIVE_POLY = 0x11D; // GF(256) için ilkel polinom
-        private readonly int[] expTable = new int[256]; // Üstel tablo (antilog)
-        private readonly int[] logTable = new int[256]; // Logaritma tablosu
+        private readonly byte[] expTable = new byte[256]; // Üstel tablo (antilog)
+        private readonly byte[] logTable = new byte[256]; // Logaritma tablosu
 
         // GF(256) alanını başlat
         private void InitializeGaloisField()
         {
-            int x = 1;
+            int value = 1;
+
+            // Generate exponentiation (antilog) and logarithm tables
             for (int i = 0; i < 255; i++)
             {
-                expTable[i] = x;
-                logTable[x] = i;
-                x <<= 1;
-                if (x >= 256) x ^= PRIMITIVE_POLY; // GF(256) için mod işlemi
-            }
-            expTable[255] = expTable[0]; // Döngüyü tamamla
-        }
+                expTable[i] = (byte)value; // Store α^i
+                logTable[value] = (byte)i; // Store log_α(value)
 
-        // Polinom çarpımı (mod 256 içinde)
-        private int[] MultiplyPolynomials(int[] poly1, int[] poly2)
-        {
-            int[] result = new int[poly1.Length + poly2.Length - 1];
+                // Multiply by primitive element (α = 0x02 in GF(256))
+                value <<= 1; // Multiply by 2 (left shift)
 
-            for (int i = 0; i < poly1.Length; i++)
-            {
-                for (int j = 0; j < poly2.Length; j++)
+                // If value overflows 8 bits, reduce using primitive polynomial (mod 0x11D)
+                if (value >= 256)
                 {
-                    result[i + j] ^= GaloisMultiply(poly1[i], poly2[j]); // GF(256) içinde çarp
+                    value ^= PRIMITIVE_POLY;
                 }
             }
 
-            return result;
+            // Duplicate last entry for easier modulo operation in lookup
+            expTable[255] = expTable[0];
+            
+            Debug.Log("Exponentiation Table (expTable):");
+            string s1 = "";
+            for (int i = 0; i < 256; i++)
+                s1 += $"{expTable[i]:X2} ";
+            Debug.Log(s1);
+            Debug.Log("Logarithm Table (logTable):");
+            string s2 = "";
+            for (int i = 0; i < 256; i++) 
+                s2 += $"{logTable[i]:X2} ";
+            Debug.Log(s2);
         }
 
         // GF(256) çarpma işlemi
-        private int GaloisMultiply(int a, int b)
+        private byte GaloisMultiply(byte a, byte b)
         {
             if (a == 0 || b == 0) return 0;
-            int sumLog = logTable[a] + logTable[b];
-            return expTable[sumLog % 255]; // Mod 255 alınır (QR standardı)
+            int logSum = logTable[a] + logTable[b];
+            return expTable[logSum % 255]; // Mod 255 alınır (QR standardı)
         }
 
-        // Reed-Solomon Üretici Polinomunu Oluştur
-        private int[] GenerateGeneratorPolynomial(int ecCodewords)
+        private byte[] GenerateGeneratorPolynomial(int numECCodewords)
         {
-            int[] generator = { 1 }; // Başlangıçta G(x) = 1
+            byte[] g = new byte[] {1}; // Start with 1 (x^0 term)
 
-            for (int i = 0; i < ecCodewords; i++)
+            for (int i = 0; i < numECCodewords; i++)
             {
-                int[] factor = { 1, expTable[i + 1] }; // (x - α^i)
-                generator = MultiplyPolynomials(generator, factor);
+                byte[] newG = new byte[g.Length + 1];
+
+                for (int j = 0; j < g.Length; j++)
+                {
+                    newG[j] ^= GaloisMultiply(g[j], expTable[i]); // (x - α^i)
+                }
+
+                newG[g.Length] = 1;
+                g = newG;
             }
 
-            return generator;
+            return g;
         }
 
-        public int[] Generate(int errorCorrectionDataBlockSize)
+        public byte[] GenerateECBlocks(byte[] data, int errorCorrectionDataBlockSize)
         {
             InitializeGaloisField();
-            int[] generatorPolynomial = GenerateGeneratorPolynomial(errorCorrectionDataBlockSize);
+            byte[] ecBlocks = ComputeErrorCorrection(data, errorCorrectionDataBlockSize);
 
-            return generatorPolynomial;
-            Debug.Log("Reed-Solomon Generator Polynomial:");
-            Debug.Log(string.Join(", ", generatorPolynomial));
+            return ecBlocks;
         }
         
-        public int[] ComputeErrorCorrectionCodewords(int[] data, int[] generatorPoly, int ecCodewords)
+        private byte[] ComputeErrorCorrection(byte[] data, int numECCodewords)
         {
-            int[] paddedData = new int[data.Length + ecCodewords];
-            Array.Copy(data, paddedData, data.Length);
+            byte[] generator = GenerateGeneratorPolynomial(numECCodewords);
+            byte[] message = new byte[data.Length + numECCodewords];
+
+            Array.Copy(data, message, data.Length); // Copy data into the message
 
             for (int i = 0; i < data.Length; i++)
             {
-                int coef = paddedData[i];
-                if (coef != 0)
+                byte coefficient = message[i];
+
+                if (coefficient != 0) // Skip zero terms
                 {
-                    int logCoef = logTable[coef];
-                    for (int j = 0; j < generatorPoly.Length; j++)
+                    for (int j = 0; j < generator.Length; j++)
                     {
-                        paddedData[i + j] ^= expTable[(logCoef + logTable[generatorPoly[j]]) % 255];
+                        message[i + j] ^= GaloisMultiply(coefficient, generator[j]);
                     }
                 }
             }
 
-            int[] errorCorrection = new int[ecCodewords];
-            Array.Copy(paddedData, data.Length, errorCorrection, 0, ecCodewords);
-            return errorCorrection;
+            // Last 'numECCodewords' are the error correction codewords
+            byte[] ecCodewords = new byte[numECCodewords];
+            Array.Copy(message, data.Length, ecCodewords, 0, numECCodewords);
+
+            return ecCodewords;
         }
     }
 }
