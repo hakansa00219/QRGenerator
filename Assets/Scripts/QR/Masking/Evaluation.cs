8ca5602aafd270a5e63459aba12d575a3ece1d66
hakansa00219 <hakansa00219@users.noreply.github.com>
2025-05-12 11:49:50 +0000

    deploy: eb8e169457676c86bd65f90a937e365fa7029fd8

--- Assets/Scripts/QR/Masking/Evaluation.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace QR.Masking
-{
-    public abstract class Evaluation
-    {
-        public abstract int Calculation(byte mask, ref Texture2D texture);
-    }
-}
--- README.md
+++
@@ -1,4 +0,0 @@
-# QRGenerator
-QR Generator with Unity
-
-![Build WebGL](https://github.com/hakansa00219/QRGenerator/actions/workflows/main.yml/badge.svg)
