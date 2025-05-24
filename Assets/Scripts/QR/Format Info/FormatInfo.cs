7c7c48d9f95a2c05764a8e90800604885ce532d6
Hakan Sarıoğlu <61359973+hakansa00219@users.noreply.github.com>
2025-05-24 16:28:13 +0300

    [skip ci] change import

--- Assets/Scripts/QR/Format Info/FormatInfo.cs
+++ Assets/Scripts/QR/Format Info/FormatInfo.cs
@@ -1,3 +1,4 @@
+using System;
 using QR.Encoding;
 using UnityEngine;
 
@@ -17,7 +18,6 @@ namespace QR
         public void SetMaskedFormatBits()
         {
             string binary = System.Convert.ToString(_maskedFormatBits, 2).PadLeft(15, '0');
-
             for (var i = 0; i < binary.Length; i++)
             {
                 bool value = binary[i] == '1';
@@ -25,10 +25,10 @@ namespace QR
                 {
                     case < 7:
                         _textureRenderer.RenderBitToTexture(8, i, value); 
-                        _textureRenderer.RenderBitToTexture(i + (int)Mathf.Floor(i / 6), 12, value);
+                        _textureRenderer.RenderBitToTexture(i + (int)MathF.Floor(i / 6), 12, value);
                         break;
                     case >= 7:
-                        _textureRenderer.RenderBitToTexture(8, 5 + i + (int)Mathf.Floor(i / 9), value); 
+                        _textureRenderer.RenderBitToTexture(8, 5 + i + (int)MathF.Floor(i / 9), value); 
                         _textureRenderer.RenderBitToTexture(6 + i, 12, value);
                         break;
                 }
