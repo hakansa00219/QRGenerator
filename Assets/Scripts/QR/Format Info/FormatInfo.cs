9d0ecbfec05d09b9ba0ecc622df2932b7b0ed65d
Hakan Sarıoğlu <61359973+hakansa00219@users.noreply.github.com>
2025-05-24 16:46:11 +0300

    [skip ci] format info fix

--- Assets/Scripts/QR/Format Info/FormatInfo.cs
+++ Assets/Scripts/QR/Format Info/FormatInfo.cs
@@ -1,6 +1,5 @@
 using System;
 using QR.Encoding;
-using UnityEngine;
 
 namespace QR
 {
@@ -18,18 +17,19 @@ namespace QR
         public void SetMaskedFormatBits()
         {
             string binary = System.Convert.ToString(_maskedFormatBits, 2).PadLeft(15, '0');
+   
             for (var i = 0; i < binary.Length; i++)
             {
                 bool value = binary[i] == '1';
                 switch (i)
-                {
+                { 
                     case < 7:
-                        _textureRenderer.RenderBitToTexture(8, i, value); 
-                        _textureRenderer.RenderBitToTexture(i + (int)MathF.Floor(i / 6), 12, value);
+                        _textureRenderer.RenderBitToTexture(8, 20 - i, value); 
+                        _textureRenderer.RenderBitToTexture(i + (int)MathF.Floor(i / 6), 8, value);
                         break;
                     case >= 7:
-                        _textureRenderer.RenderBitToTexture(8, 5 + i + (int)MathF.Floor(i / 9), value); 
-                        _textureRenderer.RenderBitToTexture(6 + i, 12, value);
+                        _textureRenderer.RenderBitToTexture(8, 15 + i + (int)MathF.Floor(i / 9), value); 
+                        _textureRenderer.RenderBitToTexture(6 + i, 8, value);
                         break;
                 }
             }
